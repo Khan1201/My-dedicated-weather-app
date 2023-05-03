4bdb2e1febc4fdaa9bb72b7ce8a57d655a63ed60
caa789 <caa789@naver.com>
2023-05-03 19:56:49 +0900

    * add very short or short term forecast model * add very short or short term forecast category converter util

--- MyDedicatedWeatherApp/Common/Util.swift
+++ MyDedicatedWeatherApp/Common/Util.swift
@@ -9,6 +9,8 @@ import Foundation
 
 struct Util {
     
+    //MARK: - For Mid Term Forecast.. (중기 예보)
+    
     func midTermForecastRequestDate() -> String {
         
         var result: String = ""
@@ -44,17 +46,161 @@ struct Util {
         
         return result
     }
+    
+    func dateToStringByAddingDay(currentDate: Date, day: Int) -> String {
+        
+        let calender: Calendar = Calendar.current
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMddHHmm"
+        
+        let dateComponent: DateComponents = DateComponents(day: day)
+        
+        let yesterdayDate: Date = calender.date(byAdding: dateComponent, to: currentDate) ?? Date()
+        let yesterdayDateToString = dateFormatter.string(from: yesterdayDate)
+        return yesterdayDateToString
+    }
+    
+    // MARK: - For Very Short or Short Term Forecast.. (초 단기 예보, 단기 예보)
+    
+    func veryShortTermForecastCategoryValue(category: VeryShortTermForecastCategory, value: String) -> String {
+        
+        switch category {
+            
+        case .T1H: // 기온
+            return "\(value)°"
+            
+        case .RN1: // 1시간 강수량
+            return remakeOneHourPrecipitationValue(value: value)
+            
+        case .PTY: // 강수 형태
+            return remakePrecipitaionTypeValue(value: value)
+            
+        case .SKY: // 하늘 상태
+            return remakeSkyStateValue(value: value)
+            
+        case .REH: // 습도
+            return "\(value)%"
+            
+        case .WSD: // 풍속
+            return remakeWindSpeedValue(value: value)
+        }
+    }
+    
+    func shortTermForecastCategoryValue(category: ShortTermForecastCategory, value: String) -> String {
+        
+        switch category {
+            
+        case .POP: // 강수 확률
+            return "\(value)%"
+            
+        case .PTY: // 강수 형태
+            return remakePrecipitaionTypeValue(value: value)
+            
+        case .PCP: // 1시간 강수량
+            return remakeOneHourPrecipitationValue(value: value)
+            
+        case .REH: // 습도
+            return "\(value)%"
+            
+        case .SKY: // 하늘 상태
+            return remakeSkyStateValue(value: value)
+            
+        case .TMP: // 1시간 기온
+            return "\(value)°"
+            
+        case .TMN: // 일 최저기온
+            return "\(value)°"
+            
+        case .TMX: // 일 최고기온
+            return "\(value)°"
+            
+        case .WSD: // 풍속
+            return remakeWindSpeedValue(value: value)
+        }
+    }
+    
+    func remakeOneHourPrecipitationValue(value: String) -> String {
+        
+        if value == "강수없음" {
+            return "비 없음"
+            
+        } else if value == "30.0~50.0mm" || value == "50.0mm 이상" {
+            return "매우 강한 비"
+            
+        } else {
+            
+            let stringToDouble: Double = Double(value.replacingOccurrences(of: "mm", with: "")) ?? 0
+            switch stringToDouble {
+                
+            case 1.0...2.9:
+                return "약한 비"
+            case 3.0...14.9:
+                return "보통 비"
+            case 15.0...29.9:
+                return "강한 비"
+            default:
+                return "알 수 없음"
+            }
+        }
+    }
+    
+    func remakePrecipitaionTypeValue(value: String) -> String {
+        
+        switch value {
+            
+        case "0":
+            return "없음"
+        case "1":
+            return "비"
+        case "2":
+            return "비/눈"
+        case "3":
+            return "눈"
+        case "5":
+            return "빗방울"
+        case "6":
+            return "빗방울 / 눈날림"
+        default:
+            return "알 수 없음"
+        }
+    }
+    
+    func remakeSkyStateValue(value: String) -> String {
+        
+        switch value {
+        case "1":
+            return "맑음"
+        case "3":
+            return "구름많음"
+        case "4":
+            return "흐림"
+        default:
+            return "알 수 없음"
+        }
+    }
+    
+    func remakeWindSpeedValue(value: String) -> String {
+        
+        let stringToDouble = Double(value) ?? 0.0
+        
+        switch stringToDouble {
+        case 0.0...3.9:
+            return "약한 바람"
+        case 4.0...8.9:
+            return "약간 강한 바람"
+        case 9.0...13.9:
+            return "강한 바람"
+        case _ where stringToDouble > 13.9:
+            return "매우 강한 바람"
+            
+        default:
+            return "알 수 없음"
+        }
+    }
+    
+    //MARK: -
 }
 
-func dateToStringByAddingDay(currentDate: Date, day: Int) -> String {
-    
-    let calender: Calendar = Calendar.current
-    let dateFormatter: DateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyyMMddHHmm"
-    
-    let dateComponent: DateComponents = DateComponents(day: day)
 
-    let yesterdayDate: Date = calender.date(byAdding: dateComponent, to: currentDate) ?? Date()
-    let yesterdayDateToString = dateFormatter.string(from: yesterdayDate)
-    return yesterdayDateToString
-}
+
+
---
+++ MyDedicatedWeatherApp/Model/OpenDataModel/VeryShortOrShortTermForecastModel.swift
@@ -0,0 +1,95 @@
+//
+//  VeryShortOrShortTermForecastModel.swift
+//  MyDedicatedWeatherApp
+//
+//  Created by 윤형석 on 2023/05/03.
+//
+
+import Foundation
+
+struct VeryShortOrShortTermForecastModel<T: Decodable>: Decodable {
+    
+    let baseDate: String
+    let baseTime: String
+    let category: T
+    let fcstDate: String
+    let fcstTime: String
+    let fcstValue: String
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case baseDate, baseTime, category,
+            fcstDate, fcstTime, fcstValue
+    }
+}
+
+extension VeryShortOrShortTermForecastModel {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.baseDate = try container.decode(String.self, forKey: .baseDate)
+        self.baseTime = try container.decode(String.self, forKey: .baseTime)
+        self.category = try container.decode(T.self, forKey: .category)
+        self.fcstDate = try container.decode(String.self, forKey: .fcstDate)
+        self.fcstTime = try container.decode(String.self, forKey: .fcstTime)
+        self.fcstValue = try container.decode(String.self, forKey: .fcstValue)
+    }
+}
+
+// MARK: - Relation ..
+
+struct VeryShortOrShortTermForecastReq: Encodable {
+    let serviceKey: String
+    let pageNo: String = "1"
+    let numOfRows: String = "1000"
+    let dataType: String = "JSON"
+    let baseDate: String
+    let baseTime: String
+    let nx: String
+    let ny: String
+    
+    enum CodingKeys: String, CodingKey {
+        case serviceKey, pageNo,
+             numOfRows, dataType,
+             baseDate = "base_date",
+             baseTime = "base_time",
+             nx, ny
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(serviceKey, forKey: .serviceKey)
+        try container.encode(pageNo, forKey: .pageNo)
+        try container.encode(numOfRows, forKey: .numOfRows)
+        try container.encode(dataType, forKey: .dataType)
+        try container.encode(baseDate, forKey: .baseDate)
+        try container.encode(baseTime, forKey: .baseTime)
+        try container.encode(nx, forKey: .nx)
+        try container.encode(ny, forKey: .ny)
+    }
+    
+}
+
+enum VeryShortTermForecastCategory: String, Codable {
+    
+    case T1H, // 기온
+    RN1, // 1시간 강수량
+    PTY, // 강수 형태
+    SKY, // 하늘 상태
+    REH, // 습도
+    WSD // 풍속
+}
+
+enum ShortTermForecastCategory: String, Codable {
+    
+    case POP, // 강수 확률
+    PTY, // 강수 형태
+    PCP, // 1시간 강수량
+    REH, // 습도
+    SKY, // 하늘상태
+    TMP, // 1시간 기온
+    TMN, // 일 최저기온
+    TMX, // 일 최고기온
+    WSD // 픙속
+    
+    
+}
