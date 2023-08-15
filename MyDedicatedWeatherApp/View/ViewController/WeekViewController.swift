ba390279f11ffdd3bd909443e9353373e01d65b1
caa789 <caa789@naver.com>
2023-08-16 00:52:03 +0900

    design: 요일, 스켈레톤(로딩), 백그라운드 등 UI 수정

--- MyDedicatedWeatherApp/View/ViewController/WeekViewController.swift
+++ MyDedicatedWeatherApp/View/ViewController/WeekViewController.swift
@@ -8,19 +8,38 @@
 import SwiftUI
 
 struct WeekViewController: View {
-    @StateObject var viewModel: WeekViewModel = WeekViewModel(weeklyWeatherInformations: [])
+    @StateObject var viewModel: WeekViewModel = WeekViewModel(weeklyWeatherInformations: Dummy().weeklyWeatherInformations())
     
     var body: some View {
+        let currentDate: Date = Date()
+        
         VStack(alignment: .leading, spacing: 0) {
-            VStack(alignment: .leading, spacing: 15) {
-                ForEach(viewModel.weeklyWeatherInformations, id: \.id) {
-                    WeekWeatherItemView(item: $0)
-                }
-            }
-        }
-        .background {
+            CurrentLocationAndDateView(
+                location: UserDefaults.standard.string(forKey: "locality") ?? "",
+                subLocation: UserDefaults.standard.string(forKey: "subLocality") ?? ""
+            )
+            .padding(.leading, 10)
+            .padding(.top, 35)
             
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .leading, spacing: 15) {
+                    ForEach(viewModel.weeklyWeatherInformations.indices, id: \.self) { index in
+                        WeekWeatherItemView(
+                            item: viewModel.weeklyWeatherInformations[index],
+                            day: currentDate.toString(byAdding: index + 1, format: "EE요일")
+                        )
+                        .padding(.bottom, index == viewModel.weeklyWeatherInformations.count - 1 ? 45 : 0)
+                    }
+                }
+                .loadingProgressLottie(isLoadingCompleted: viewModel.isWeeklyWeatherInformationsLoaded)
+            }
+            .padding(.top, 24)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .weekViewControllerBackground(
+            isDayMode: UserDefaults.standard.bool(forKey: "isDayMode"),
+            skyKeyword: UserDefaults.standard.string(forKey: "skyKeyword") ?? ""
+        )
         .task {
             await viewModel.performWeekRequests()
         }
@@ -30,8 +49,5 @@ struct WeekViewController: View {
 struct WeekViewController_Previews: PreviewProvider {
     static var previews: some View {
         WeekViewController(viewModel: WeekViewModel(weeklyWeatherInformations: Dummy.shared.weeklyWeatherInformations()))
-            .onAppear {
-                
-            }
     }
 }
