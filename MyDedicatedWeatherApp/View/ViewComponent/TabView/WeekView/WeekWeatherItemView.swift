cd6b7ebc9b4583d0ebae64b9e3492ff834c1d1cf
caa789 <caa789@naver.com>
2023-08-16 00:47:09 +0900

    design: 그라데이션, 백그라운드 컬러, 애니메이션 적용 및 크기 수정

--- MyDedicatedWeatherApp/View/ViewComponent/TabView/WeekView/WeekWeatherItemView.swift
+++ MyDedicatedWeatherApp/View/ViewComponent/TabView/WeekView/WeekWeatherItemView.swift
@@ -9,50 +9,60 @@ import SwiftUI
 
 struct WeekWeatherItemView: View {
     let item: Weather.WeeklyWeatherInformation
+    let day: String
     @State var recWidth: CGFloat = 0
     
     var body: some View {
+        
         HStack(alignment: .center, spacing: 0) {
-            Text("일요일")
-                .fontSpoqaHanSansNeo(size: 18, weight: .medium)
+            Text(day)
+                .fontSpoqaHanSansNeo(size: 16, weight: .medium)
                 .foregroundColor(Color.white)
             
             Image("\(item.weatherImage)")
                 .resizable()
-                .frame(width: 40, height: 40)
+                .frame(width: 35, height: 35)
                 .if(item.rainfallPercent != "0") { view in
                     view
                         .overlay(alignment: .bottom) {
                             Text(item.rainfallPercent + "%")
-                                .fontSpoqaHanSansNeo(size: 10, weight: .bold)
+                                .fontSpoqaHanSansNeo(size: 13, weight: .bold)
                                 .foregroundColor(CustomColor.lightBlue.toColor)
                                 .offset(y: 4)
                         }
                 }
                 .padding(.leading, 25)
+            
+            Text("\(item.minTemperature)°")
+                .fontSpoqaHanSansNeo(size: 18, weight: .bold)
+                .foregroundColor(Color.gray.opacity(0.5))
+                .padding(.leading, 12)
                         
             VStack(alignment: .leading, spacing: 0) {
                 RoundedRectangle(cornerRadius: 16)
-                    .fill(Color.blue.opacity(0.3))
+                    .fill(LinearGradient(colors: [Color(hexCode: "CDCF5C"), Color(hexCode: "EF8835")], startPoint: .leading, endPoint: .trailing))
                     .frame(width: recWidth, height: 5)
-                    .padding(.leading, 14)
+                    .padding(.leading, 12)
             }
-            .frame(width: 122, alignment: .leading)
+            .frame(width: 112, alignment: .leading)
             
             Text("\(item.maxTemperature)°")
                 .fontSpoqaHanSansNeo(size: 18, weight: .bold)
                 .foregroundColor(Color.white)
-                .padding(.leading, 14)
+                .padding(.leading, 12)
             
         }
-        .frame(width: UIScreen.screenWidth - 48)
+        .frame(maxWidth: UIScreen.screenWidth - 70)
         .padding(.horizontal, 14)
         .padding(.vertical, 8)
-        .background(Color.white.opacity(0.08))
+        .background {
+            UserDefaults.standard.bool(forKey: "isDayMode") ? CustomColor.lightNavy.toColor.opacity(0.2) : Color.white.opacity(0.08)
+        }
         .cornerRadius(14)
-        .task {
-            withAnimation(.linear(duration: 0.8)) {
-                recWidth = CGFloat(item.maxTemperature.toInt) * 3.3
+        .onAppear {
+            recWidth = 0
+            withAnimation(.linear(duration: 1)) {
+                recWidth = CGFloat(item.maxTemperature.toInt) * 3
             }
         }
     }
@@ -60,6 +70,6 @@ struct WeekWeatherItemView: View {
 
 struct WeekWeatherItemView_Previews: PreviewProvider {
     static var previews: some View {
-        WeekWeatherItemView(item: Dummy.shared.weeklyWeatherInformation())
+        WeekWeatherItemView(item: Dummy.shared.weeklyWeatherInformation(), day: "월요일")
     }
 }
