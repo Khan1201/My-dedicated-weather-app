d8fbe51880455df5734d9b1d2c1cdc027d90fca1
caa789 <caa789@naver.com>
2023-10-22 22:09:11 +0900

    Base model 내부 Req 분리

--- MyDedicatedWeatherApp/Models/Base/PublicData/DustForecast/DustForecastStationXYBase.swift
+++ MyDedicatedWeatherApp/Models/Base/PublicData/DustForecast/DustForecastStationXYBase.swift
@@ -32,25 +32,3 @@ struct DustForecastStationXYBase: Decodable {
         self.sidoName = try container.decode(String.self, forKey: .sidoName)
     }
 }
-
-struct DustForecastStationXYReq: Encodable {
-    
-    let serviceKey: String
-    let returnType: String = "json"
-    let numOfRows: String = "100"
-    let pageNo: String = "1"
-    let umdName: String
-    
-    enum CodingKeys: String, CodingKey {
-        case serviceKey, returnType, numOfRows, pageNo, umdName
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(serviceKey.self, forKey: .serviceKey)
-        try container.encode(returnType.self, forKey: .returnType)
-        try container.encode(numOfRows.self, forKey: .numOfRows)
-        try container.encode(pageNo.self, forKey: .pageNo)
-        try container.encode(umdName, forKey: .umdName)
-    }
-}
