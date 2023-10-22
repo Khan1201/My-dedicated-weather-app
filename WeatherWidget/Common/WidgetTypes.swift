a9764c180c79c0de465f3b1f5a34790d4af9f4ff
caa789 <caa789@naver.com>
2023-10-22 22:13:25 +0900

    초단기, 단기 예보 요청 response 값 타입 추가

--- WeatherWidget/Common/WidgetTypes.swift
+++ WeatherWidget/Common/WidgetTypes.swift
@@ -12,100 +12,37 @@ enum MidtermReqType {
     case temperature, skystate, news
 }
 
-//struct Weather {
-//    
-//    struct DescriptionAndSkyTypeAndImageString {
-//        let description: String
-//        let skyType: SkyType
-//        let imageString: String
-//    }
-//
-//    struct DescriptionAndColor: Equatable {
-//        let description: String
-//        let color: Color
-//    }
-//
-//    struct TodayInformation {
-//        let time: String
-//        let weatherImage: String
-//        let precipitation: String
-//        let temperature: String
-//    }
-//
-//    struct CurrentInformation {
-//        let temperature: String
-//        let windSpeed: (String, String)
-//        let wetPercent: (String, String)
-//        let oneHourPrecipitation: (String, String)
-//        let weatherImage: String
-//        let skyType: SkyType
-//    }
-//
-//    struct WeeklyInformation: Identifiable {
-//        let id = UUID()
-//        let weatherImage: String
-//        let rainfallPercent: String
-//        let minTemperature: String
-//        let maxTemperature: String
-//    }
-//
-//    struct WeeklyChartInformation {
-//        let minTemps: [CGFloat]
-//        let maxTemps: [CGFloat]
-//        let xList: [(String, String)] // x축 = (요일, 날짜)
-//        let yList: [Int] // y축 = 온도 범위
-//        let imageAndRainPercents: [(String, String)]
-//    }
-//
-//    enum SkyType: String {
-//        case sunny,
-//             cloudy,
-//             blur,
-//             rainy,
-//             snow,
-//             thunder,
-//             none
-//
-//        var backgroundImageKeyword: String {
-//
-//            switch self {
-//
-//            case .sunny:
-//                return "sunny"
-//
-//            case .cloudy, .blur, .rainy, .snow, .thunder:
-//                return "blur"
-//
-//            case .none:
-//                return ""
-//            }
-//        }
-//
-//        var backgroundLottieKeyword: String {
-//
-//            switch self {
-//
-//            case .sunny:
-//                return "Sunny"
-//
-//            case .cloudy:
-//                return "Cloudy"
-//
-//            case .blur:
-//                return ""
-//
-//            case .rainy:
-//                return "Rainy"
-//
-//            case .snow:
-//                return "Snow"
-//
-//            case .thunder:
-//                return "Thunder"
-//
-//            case .none:
-//                return ""
-//            }
-//        }
-//    }
-//}
+// MARK: - 초단기예보, 단기예보 값 types
+
+enum VeryShortTermForecastCategory: String, Codable {
+    
+    case T1H, // 기온
+         RN1, // 1시간 강수량
+         PTY, // 강수 형태
+         SKY, // 하늘 상태
+         REH, // 습도
+         WSD,// 풍속
+         UUU, // 동서 바람 성분 (사용 x)
+         VVV, // 남북 바람 성분 (사용 x),
+         LGT, // 낙뢰 (사용 x)
+         VEC // 풍향 (사용 x)
+}
+
+enum ShortTermForecastCategory: String, Codable {
+    
+    case POP, // 강수 확률
+         PTY, // 강수 형태
+         PCP, // 1시간 강수량
+         REH, // 습도
+         SNO, // 1시간 신적설 (사용 x)
+         SKY, // 하늘상태
+         TMP, // 1시간 기온
+         TMN, // 일 최저기온
+         TMX, // 일 최고기온
+         UUU, // 동서 바람 성분 (사용 x)
+         VVV, // 남북 바람 성분 (사용 x),
+         WAV, // 파고 (사용 x)
+         VEC, // 풍향 (사용 x)
+         WSD // 풍속
+}
+
