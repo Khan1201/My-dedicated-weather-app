07d64405a94082041a47d3daaada7a94289ca023
caa789 <caa789@naver.com>
2023-11-30 00:18:38 +0900

    카카오 주소 Api Model - 전체주소 추가

--- MyDedicatedWeatherApp/Models/Base/KakaoAddress/KakaoAddressBase.swift
+++ MyDedicatedWeatherApp/Models/Base/KakaoAddress/KakaoAddressBase.swift
@@ -8,15 +8,17 @@
 import Foundation
 
 struct KakaoAddressBase: Encodable {
-    
+    let fullAddress: String
     let subLocality: String // 성수동 1가
     
     enum CodingKeys: String, CodingKey {
+        case address_name
         case region_3depth_name
     }
     
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(fullAddress, forKey: .address_name)
         try container.encode(subLocality, forKey: .region_3depth_name)
     }
     
@@ -34,6 +36,7 @@ struct KakaoAddressBase: Encodable {
 extension KakaoAddressBase: Decodable {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.fullAddress = try container.decode(String.self, forKey: .address_name)
         self.subLocality = try container.decode(String.self, forKey: .region_3depth_name)
     }
 }
