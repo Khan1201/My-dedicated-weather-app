ea87b4802e1d69db281d489061de941194b368a7
caa789 <caa789@naver.com>
2023-12-05 00:30:32 +0900

    추가 위치 view - 로딩 view 추가

--- MyDedicatedWeatherApp/Views/Pages/AdditionalLocation/AdditionalLocationView.swift
+++ MyDedicatedWeatherApp/Views/Pages/AdditionalLocation/AdditionalLocationView.swift
@@ -16,31 +16,49 @@ struct AdditionalLocationView: View {
     @State private var navNextView: Bool = false
     
     var body: some View {
+        let isLoading: Bool = progress == .loading
         
-        VStack(alignment: .leading, spacing: 0) {
-            CustomNavigationBar(isPresented: $isPresented, isNavigationUsed: false, title: "추가 위치")
+        ZStack {
+            VStack(alignment: .leading, spacing: 0) {
+                CustomNavigationBar(
+                    isPresented: $isPresented,
+                    isNavigationUsed: false,
+                    title: "추가 위치"
+                )
                 .padding(.top, 10)
-            
-            AdditionalLocationSavedListView(savedAddresses: vm.savedAddress, tempItems: vm.tempItems)
+                
+                AdditionalLocationSavedListView(
+                    fullAddresses: vm.fullAddresses,
+                    localities: vm.localities,
+                    subLocalities: vm.subLocalities,
+                    tempItems: vm.tempItems,
+                    itemOnTapGesture: finalLocationOnTapGesture, 
+                    itemDeleteAction: vm.itemDeleteAction(fullAddress:locality:subLocality:)
+                )
                 .padding(.top, 20)
                 .padding(.horizontal, 20)
-
-            
-            HStack(alignment: .center, spacing: 5) {
-                Image(systemName: "plus.magnifyingglass")
-                    .resizable()
-                    .frame(width: 15, height: 15)
                 
-                Text("추가하기")
-                    .fontSpoqaHanSansNeo(size: 15, weight: .medium)
+                HStack(alignment: .center, spacing: 5) {
+                    Image(systemName: "plus.magnifyingglass")
+                        .resizable()
+                        .frame(width: 15, height: 15)
+                    
+                    Text("추가하기")
+                        .fontSpoqaHanSansNeo(size: 15, weight: .medium)
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.top, 25)
+                .onTapGesture {
+                    navNextView = true
+                }
+                
+                Spacer()
             }
-            .frame(maxWidth: .infinity)
-            .padding(.top, 25)
-            .onTapGesture {
-                navNextView = true
-            }
+            .opacity(isLoading ? 0.4 : 1)
             
-            Spacer()
+            if isLoading {
+                ProgressView()
+            }
         }
         .preferredColorScheme(.dark)
         .task {
