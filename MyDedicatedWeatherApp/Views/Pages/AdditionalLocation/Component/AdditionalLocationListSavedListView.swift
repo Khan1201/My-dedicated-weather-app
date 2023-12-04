cc3e00d46e8ae85871ffec84e0c1311cecb2f469
caa789 <caa789@naver.com>
2023-12-05 00:31:33 +0900

    추가 위치 - 아이템 삭제 action 연결, 드래그 시 삭제 View 나오도록 추가

--- MyDedicatedWeatherApp/Views/Pages/AdditionalLocation/Component/AdditionalLocationListSavedListView.swift
+++ MyDedicatedWeatherApp/Views/Pages/AdditionalLocation/Component/AdditionalLocationListSavedListView.swift
@@ -8,31 +8,57 @@
 import SwiftUI
 
 struct AdditionalLocationSavedListView: View {
-    let savedAddresses: [String]
+    let fullAddresses: [String]
+    let localities: [String]
+    let subLocalities: [String]
     let tempItems: [Weather.WeatherImageAndMinMax]
+    let itemOnTapGesture: ((String, String, String, Bool) -> Void)
+    let itemDeleteAction: (String, String, String) -> Void
+    
     
     var body: some View {
         
         VStack(alignment: .leading, spacing: 10) {
-            ForEach(savedAddresses.indices, id: \.self) { i in
-                AdditionalLocationSavedListItemView(address: savedAddresses[i], tempItem: tempItems[i])
+            ForEach(fullAddresses.indices, id: \.self) { i in
+                AdditionalLocationSavedListItemView(
+                    fullAddress: fullAddresses[i],
+                    locality: localities[i],
+                    subLocality: subLocalities[i],
+                    tempItem: tempItems[i],
+                    onTapGesture: itemOnTapGesture,
+                    deleteAction: itemDeleteAction
+                )
             }
         }
     }
 }
 
 #Preview {
-    AdditionalLocationSavedListView(savedAddresses: [], tempItems: [])
+    AdditionalLocationSavedListView(
+        fullAddresses: [],
+        localities: [],
+        subLocalities: [],
+        tempItems: [],
+        itemOnTapGesture: { _, _, _, _ in }, 
+        itemDeleteAction: {_, _, _ in }
+    )
 }
 
 struct AdditionalLocationSavedListItemView: View {
-    let address: String
+    let fullAddress: String
+    let locality: String
+    let subLocality: String
     let tempItem: Weather.WeatherImageAndMinMax
+    let onTapGesture: ((String, String, String, Bool) -> Void)
+    let deleteAction: (String, String, String) -> Void
+    
+    @State private var isDeleteMode: Bool = false
+    @State private var itemSize: CGSize = CGSize()
     
     var body: some View {
         
         HStack(alignment: .center, spacing: 0) {
-            Text(address)
+            Text(fullAddress)
                 .fontSpoqaHanSansNeo(size: 15, weight: .medium)
                 .foregroundStyle(Color.white)
             
@@ -47,17 +73,62 @@ struct AdditionalLocationSavedListItemView: View {
                 Text(" \(tempItem.currentTemp)°")
                     .fontSpoqaHanSansNeo(size: 18, weight: .medium)
                     .foregroundStyle(Color.white)
-
+                
                 Text("\(tempItem.minMaxTemp.0)°/\(tempItem.minMaxTemp.1)°")
                     .fontSpoqaHanSansNeo(size: 12, weight: .regular)
                     .foregroundStyle(Color.white.opacity(0.7))
             }
+            .padding(.vertical, 12)
             .padding(.leading, 5)
+            .padding(.trailing, 15)
             .loadingProgressLottie(isLoadingCompleted: tempItem.currentTemp != "" && tempItem.minMaxTemp != ("", ""))
+            
+            if isDeleteMode {
+                VStack(alignment: .leading, spacing: 0) {
+                    Rectangle()
+                        .fill(Color.red)
+                        .frame(width: 70, height: itemSize.height)
+                        .overlay {
+                            Text("삭제")
+                                .fontSpoqaHanSansNeo(size: 14, weight: .medium)
+                                .foregroundStyle(Color.white)
+                        }
+                }
+                .transition(.move(edge: .trailing))
+                .onTapGesture {
+                    deleteAction(fullAddress, locality, subLocality)
+                    isDeleteMode = false
+                }
+            }
         }
-        .padding(.vertical, 12)
-        .padding(.horizontal, 15)
+        .padding(.leading, 15)
         .background(Color.white.opacity(0.1))
         .clipShape(RoundedRectangle(cornerRadius: 8))
+        .getSize(size: $itemSize)
+        .onTapGesture {
+            if isDeleteMode {
+                withAnimation(.easeIn(duration: 0.15)) {
+                    isDeleteMode = false
+                }
+                
+            } else {
+                onTapGesture(fullAddress, locality, subLocality, false)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    if value.translation.width < 0 {
+                        withAnimation(.easeIn(duration: 0.15)) {
+                            isDeleteMode = true
+                        }
+                        
+                    } else {
+                        withAnimation(.easeIn(duration: 0.15)) {
+                            isDeleteMode = false
+                        }
+                    }
+                }
+        )
     }
 }
