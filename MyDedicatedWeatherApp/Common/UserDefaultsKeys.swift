4d333a02271795a6d0b8515bd3efd1af87b90d94
caa789 <caa789@naver.com>
2023-12-05 00:23:57 +0900

    UserDefaulsKeys.swift 추가

---
+++ MyDedicatedWeatherApp/Common/UserDefaultsKeys.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaultsKeys.swift
+//  MyDedicatedWeatherApp
+//
+//  Created by 윤형석 on 12/4/23.
+//
+
+import Foundation
+
+struct UserDefaultsKeys {
+    
+    static let additionalFullAddresses: String = "additionalFullAddresses"
+    static let additionalLocalities: String = "additionalLocalities"
+    static let additionalSubLocalities: String = "additionalSubLocalities"
+    static let fullAddress: String = "fullAddress"
+}
