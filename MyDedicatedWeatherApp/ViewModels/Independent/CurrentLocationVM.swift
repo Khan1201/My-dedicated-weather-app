024d86f90622c9a1428589889c61674fdb7c5712
caa789 <caa789@naver.com>
2023-12-06 01:12:38 +0900

    현재 위치 싱글턴 VM 추가

--- MyDedicatedWeatherApp/ViewModels/Independent/CurrentLocationVM.swift
+++ MyDedicatedWeatherApp/ViewModels/Independent/CurrentLocationVM.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+final class CurrentLocationVM: ObservableObject {
+    
+    static let shared = CurrentLocationVM()
+    
+    @Published private(set) var latitude: String = ""
+    @Published private(set) var longitude: String = ""
+    @Published private(set) var xy: (String, String) = ("", "")
+    @Published private(set) var locality: String = ""
+    @Published private(set) var subLocality: String = ""
+    @Published private(set) var fullAddress: String = ""
+    
+    private init() {}
+}
+
+// MARK: - Set funcs..
+
+extension CurrentLocationVM {
+    func setLocality(_ value: String) {
+        self.locality = value
+    }
+    
+    func setSubLocality(_ value: String) {
+        self.subLocality = value
+    }
+    
+    func setFullAddress(_ value: String) {
+        self.fullAddress = value
+    }
+    
+    func setXY(_ value: (String, String)) {
+        self.xy = value
+    }
+    
+    func setLatitude(_ value: String) {
+        self.latitude = value
+    }
+    
+    func setLongitude(_ value: String) {
+        self.longitude = value
+    }
+}
