e9ad7a41759749fd0c81d8e6cc0997117dcba052
caa789 <caa789@naver.com>
2023-12-06 01:11:27 +0900

    UserDefaultsKeys - 변수 추가 - locality - subLocality - dustStation - latitude - longitude - x - y

--- MyDedicatedWeatherApp/Common/UserDefaultsKeys.swift
+++ MyDedicatedWeatherApp/Common/UserDefaultsKeys.swift
@@ -12,5 +12,12 @@ struct UserDefaultsKeys {
     static let additionalFullAddresses: String = "additionalFullAddresses"
     static let additionalLocalities: String = "additionalLocalities"
     static let additionalSubLocalities: String = "additionalSubLocalities"
+    static let locality: String = "locality"
+    static let subLocality: String = "subLocality"
     static let fullAddress: String = "fullAddress"
+    static let dustStation: String = "dustStation"
+    static let latitude: String = "latitude"
+    static let longitude: String = "longitude"
+    static let x: String = "x"
+    static let y: String = "y"
 }
---
+++ MyDedicatedWeatherApp/ViewModels/Independent/CurrentLocationVM.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentLocationVM.swift
+//  MyDedicatedWeatherApp
+//
+//  Created by 윤형석 on 12/5/23.
+//
+
+import Foundation
