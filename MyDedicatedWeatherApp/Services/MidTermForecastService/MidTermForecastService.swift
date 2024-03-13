699de212a7e289eedb411b61e22a534766fc564b
caa789 <caa789@naver.com>
2024-03-13 23:13:15 +0900

    chore: service 폴더링

---
+++ MyDedicatedWeatherApp/Services/MidTermForecastService/MidTermForecastService.swift
@@ -0,0 +1,8 @@
+//
+//  MidTermForecastService.swift
+//  MyDedicatedWeatherApp
+//
+//  Created by 윤형석 on 3/13/24.
+//
+
+import Foundation
---
+++ MyDedicatedWeatherApp/Services/ShortForecast/ShortForecastService.swift
@@ -0,0 +1,8 @@
+//
+//  ShortForecastService.swift
+//  MyDedicatedWeatherApp
+//
+//  Created by 윤형석 on 3/13/24.
+//
+
+import Foundation
