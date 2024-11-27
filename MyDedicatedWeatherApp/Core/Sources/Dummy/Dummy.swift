2d83ec26e74687fd696213064c6c176096b7b57e
caa789 <caa789@naver.com>
2024-11-28 01:29:28 +0900

    refactor: Dummy 타입 수정

--- MyDedicatedWeatherApp/Core/Sources/Dummy/Dummy.swift
+++ MyDedicatedWeatherApp/Core/Sources/Dummy/Dummy.swift
@@ -19,7 +19,7 @@ public final class Dummy {
             wetPercent: ("", ""),
             oneHourPrecipitation: ("", ""),
             weatherImage: "",
-            skyType: .cloudy
+            skyType: SkyType.cloudy
         )
     }
     
