47cba2c006f80b5ea9d76f9d270ecbec5fe7a630
caa789 <caa789@naver.com>
2024-11-28 01:00:55 +0900

    feat: APIValue protocol 변수 및 함수 추가, 초기구현

--- MyDedicatedWeatherApp/Domain/Sources/Util/APIValueConverter/APIValueConverter.swift
+++ MyDedicatedWeatherApp/Domain/Sources/Util/APIValueConverter/APIValueConverter.swift
@@ -13,4 +13,18 @@ protocol APIValueConverter {
 
 public protocol APIValue {
     var toDescription: String { get }
+    var backgroundImage: String { get }
+    func backgroundLottie(isDayMode: Bool) -> String
+    func image(isDayMode: Bool) -> String
+    func lottie(isDayMode: Bool) -> String
+    func lottieOffset(isDayMode: Bool) -> Double
+    
+}
+
+extension APIValue {
+    public var backgroundImage: String { return "" }
+    public func backgroundLottie(isDayMode: Bool) -> String { return "" }
+    public func image(isDayMode: Bool) -> String { return "" }
+    public func lottie(isDayMode: Bool) -> String { return "" }
+    public func lottieOffset(isDayMode: Bool) -> Double { return 0 }
 }
