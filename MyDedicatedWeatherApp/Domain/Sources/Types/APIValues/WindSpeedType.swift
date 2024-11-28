db3cdd00a54988cf3a14243621da8ffceb184319
caa789 <caa789@naver.com>
2024-11-29 00:45:42 +0900

    feat: WindSppedType 추가 (APIValue protocol 채택)

---
+++ MyDedicatedWeatherApp/Domain/Sources/Types/APIValues/WindSpeedType.swift
@@ -0,0 +1,27 @@
+//
+//  WindSpeedType.swift
+//
+//
+//  Created by 윤형석 on 11/29/24.
+//
+
+import Foundation
+
+public enum WindSpeedType: APIValue {
+    case weak, littleStrong, strong, veryStrong, none
+    
+    public var toDescription: String {
+        switch self {
+        case .weak:
+            return "약한 바람"
+        case .littleStrong:
+            return "약간 강한 바람"
+        case .strong:
+            return "강한 바람"
+        case .veryStrong:
+            return "매우 강한 바람"
+        case .none:
+            return "알 수 없음"
+        }
+    }
+}
