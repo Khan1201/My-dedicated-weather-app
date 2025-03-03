d8f754fc152abcdec905345547cebaa501fcc1d3
caa789 <caa789@naver.com>
2025-03-03 15:48:59 +0900

    refactor: ContentEO - 필요없는 변수 제거, 접근지정자 수정
    - isRefreshed 변수 제거
    - isLaunchScreenPresented 접근 지정자 private(set)으로 수정

--- MyDedicatedWeatherApp/Core/Sources/EnvironmentObjects/ContentEO.swift
+++ MyDedicatedWeatherApp/Core/Sources/EnvironmentObjects/ContentEO.swift
@@ -10,17 +10,12 @@ import Domain
 
 public final class ContentEO: ObservableObject {
     @Published public var currentTab: TabBarType = .current
-    @Published public var isLaunchScreenPresented: Bool = true
+    @Published private(set) public var isLaunchScreenPresented: Bool = true
     @Published public var isTabBarTouchDisabled: Bool = true
     @Published public var isTabBarTouchNoticeFloaterPresented: Bool = false
-    @Published public var isRefreshed: Bool = false
           
     public init() {}
-}
-
-// MARK: - On tap gestures..
-
-extension ContentEO {
+    
     public func tabBarItemOnTapGesture(_ type: TabBarType) {
         if isTabBarTouchDisabled {
             isTabBarTouchNoticeFloaterPresented = true
@@ -29,26 +24,10 @@ extension ContentEO {
             currentTab = type
         }
     }
-}
-
-// MARK: - Life cycle funcs..
-
-extension ContentEO {
+    
     public func hideLaunchScreenAfterFewSeconds() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
             self.isLaunchScreenPresented = false
-        }
-    }
-}
-
-// MARK: - Set funcs..
-
-extension ContentEO {
-    public func setIsRefreshedActionWhenRefreshStart() {
-        isRefreshed = true
-        
-        DispatchQueue.main.asyncAfter(wallDeadline: .now() + 0.5) {
-            self.isRefreshed = false
         }
     }
 }
